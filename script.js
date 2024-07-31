2c40d6b6af0c692b1399cdba1c1b1c2544977e14
Analice da Mata <142676052+Aninha8925@users.noreply.github.com>
2024-07-31 14:15:35 -0300

    Termino da landingPage
    Ainda falta fazer parte de responsivo e fazer os modais, a qual já tem um começo.

---
+++ script.js
@@ -0,0 +1,23 @@
+function direita() {
+    let slides = document.getElementById('slides');
+    slides.style.marginLeft = '-70vw';
+    if (slides.style.marginLeft == '-70vw') {
+        slides.style.marginLeft = '70vw'
+    }
+}
+
+function esquerda() {
+    let slides = document.getElementById('slides');
+    slides.style.marginLeft = '70vw';
+    if (slides.style.marginLeft == '70vw') {
+        slides.style.marginLeft = '-70vw'
+    }
+}
+function modalentrar() {
+
+}
+
+function modalcadastrar() {
+    
+}
+
